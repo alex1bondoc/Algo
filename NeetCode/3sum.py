e482803af000fbc59c61a9ed5c97d7d93c6308d0
alex1bondoc <alex1bondoc@gmail.com>
2026-03-19 18:19:52 +0200

    3 sum

--- NeetCode/3sum.py
+++ NeetCode/3sum.py
@@ -1,22 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
-        for i in range(len(nums)):
+        i = 0 
+        res = []
+        while i < len(nums):
             if nums[i] > 0:
                 break
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-            l, r = i + 1, len(nums) - 1
+            l = i + 1
+            r = len(nums) - 1
             while l < r:
-                if nums[l] + nums[r] + nums[i] == 0:
+                sum = nums[i] + nums[l] + nums[r]
+                if sum == 0:
                     res.append([nums[i], nums[l], nums[r]])
                     l += 1
                     r -= 1
-                    while nums[l] == nums[l - 1] and l < r:
+                    while l < len(nums) and nums[l] == nums[l - 1]:
                         l += 1
-                elif nums[l] + nums[r] + nums[i] > 0:
+                elif sum > 0:
                     r -= 1
                 else :
                     l += 1
+            i += 1
+            while i < len(nums) and nums[i] == nums[i - 1]:
+                i+=1
         return res
