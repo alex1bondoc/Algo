ca13342fb01958f25c5ab63b57db62f9b497b45c
alex1bondoc <alex1bondoc@gmail.com>
2026-03-19 17:04:24 +0200

    Valid Paranthesis

--- NeetCode/ValidParanthesisString.py
+++ NeetCode/ValidParanthesisString.py
@@ -1,15 +1,30 @@
-class Solution:
-    def checkValidString(self, s: str) -> bool:
-        leftMin, leftMax = 0, 0
-        for c in s:
-            if c == "(":
-                leftMin, leftMax = leftMin + 1, leftMax + 1
-            elif c == ")":
-                leftMin, leftMax = leftMin - 1, leftMax - 1
-            else:
-                leftMin, leftMax = leftMin - 1, leftMax + 1
-            if leftMax < 0:
-                return False
-            if leftMin < 0:
-                leftMin = 0
-        return leftMin == 0
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for (auto c : s) {
+            if (c == ')') {
+                if (st.empty() || st.top() != '(')
+                    return false;
+                st.pop();
+            }
+            else if (c == ']') {
+                if (st.empty() || st.top() != '[')
+                    return false;
+                st.pop();
+                
+            }
+            else if (c == '}') {
+                if (st.empty() || st.top() != '{')
+                    return false;
+                st.pop();
+                
+            }
+            else {
+                st.push(c);
+            }
+        }
+        return st.empty();
+    }
+};
