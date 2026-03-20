b06cf0677c76e9e790580997d17b824690922045
alex1bondoc <alex1bondoc@gmail.com>
2026-03-20 10:26:38 +0200

    Car fleet

--- NeetCode/CarFleet.py
+++ NeetCode/CarFleet.py
@@ -2,13 +2,9 @@ class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         cars = [(p, s) for p, s in zip(position, speed)]
         cars.sort(reverse=True)
-        fleets = 1
         stack = []
-        time = (target - cars[0][0])/ cars[0][1] 
-        for i in range(1, len(cars)):
-            p, s = cars[i]
-            curTime = (target - p) / s
-            if time < curTime:
-                fleets += 1
-                time = curTime
-        return fleets
+        for (p, s) in cars:
+            stack.append((target - p)/ s)
+            while len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
