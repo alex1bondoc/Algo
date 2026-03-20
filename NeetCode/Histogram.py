b25e57144826c4431c9761cc88287d56750a1d7f
alex1bondoc <alex1bondoc@gmail.com>
2026-03-20 11:00:26 +0200

    Histogram

--- NeetCode/Histogram.py
+++ NeetCode/Histogram.py
@@ -2,27 +2,16 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         n = len(heights)
         stack = []
-
-        leftMost = [-1] * n
-        for i in range(n):
-            while stack and heights[stack[-1]] >= heights[i]:
-                stack.pop()
-            if stack:
-                leftMost[i] = stack[-1]
-            stack.append(i)
-
-        stack = []
-        rightMost = [n] * n
-        for i in range(n - 1, -1, -1):
-            while stack and heights[stack[-1]] >= heights[i]:
-                stack.pop()
-            if stack:
-                rightMost[i] = stack[-1]
-            stack.append(i)
-
         maxArea = 0
+
         for i in range(n):
-            leftMost[i] += 1
-            rightMost[i] -= 1
-            maxArea = max(maxArea, heights[i] * (rightMost[i] - leftMost[i] + 1))
+            while stack and heights[i] <= heights[stack[-1]]:
+                height = heights[stack.pop()]
+                width = i if not stack else i - stack[-1] - 1
+                maxArea = max(maxArea, height * width)
+            stack.append(i)
+        while stack:
+            height = heights[stack.pop()]
+            width = n if not stack else n - stack[-1] - 1
+            maxArea = max(maxArea, height * width)
         return maxArea
