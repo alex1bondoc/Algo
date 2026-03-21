9fe19acb9f5cc020059e06d06ac5ddd27a81d615
alex1bondoc <alex1bondoc@gmail.com>
2026-03-21 16:43:35 +0200

    Min stack recap

--- NeetCode/MinStack.py
+++ NeetCode/MinStack.py
@@ -1,42 +1,35 @@
 
-class MinStack {
-private:
-    stack<long> st;
-    long mini = 2147483647;
-public:
-    
-    MinStack() {}
-    
-    void push(int val) {
-        if (st.empty()) {
-            st.push(0);
-            mini = val;
-        }
-        else {
-            st.push(val - mini);
-            if (val < mini) {
-                mini = val;
-            }
-        }
-    }
-    void pop() {
-        if (st.empty()) 
-            return;
-        if (st.top() >=0) {
-            st.pop();
-        }
-        else {
-            mini = mini - st.top();
-            st.pop();
-        }
-    }
-    
-    int top() {
-        if (st.top() > 0) return st.top() + mini;
-        else return mini;
-    }
-    
-    int getMin() {
-        return mini;
-    }
-};
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min = float("inf")
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.min = val
+            self.stack.append(0)
+        else :
+            self.stack.append(val - self.min)
+            if val < self.min:
+                self.min = val
+
+
+    def pop(self) -> None:
+        if not self.stack:
+            return
+        top = self.stack.pop()
+        if top < 0:
+            self.min = self.min - top
+        
+
+    def top(self) -> int:
+        if not self.stack:
+            return 
+        top = self.stack[-1]
+        if top <= 0:
+            return self.min
+        return top + self.min
+
+    def getMin(self) -> int:
+        return self.min
