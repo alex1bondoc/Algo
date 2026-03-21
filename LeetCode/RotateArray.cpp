be83b9077224569cf3327dfe38f8aa8f966fbdf9
alex1bondoc <alex1bondoc@gmail.com>
2026-03-21 15:05:57 +0200

    Rotate array recap

--- LeetCode/RotateArray.cpp
+++ LeetCode/RotateArray.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        if ( k > n)
+        if (k > n) {
             k = k % n;
-              
-        for (int i = 0; i < n / 2; ++i) {
+        }
+        for (int i = 0; i < n / 2; ++ i) {
             swap(nums[i], nums[n - 1 - i]);
         }
-        for (int i = 0; i < n && i < k / 2; ++i){
-            swap(nums[i], nums[k - 1 - i]);
+        for (int i = 0;i < k / 2; ++i) {
+            swap(nums[i], nums[k - 1- i]);
         }
-        for (int i = k; i < (n + k) / 2; ++i) {
-            swap(nums[i], nums[n + k - 1 - i]);
+        for (int i = 0; i < (n - k) / 2; ++i) {
+            swap(nums[k + i], nums[n - 1 - i]);
         }
+        
     }
 };
