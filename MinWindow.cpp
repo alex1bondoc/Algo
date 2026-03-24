c6ccc3fd1e1fa32ded1a2bf281e5a97ec580b0fa
alex1bondoc <alex1bondoc@gmail.com>
2026-03-25 00:04:55 +0200

    Min window recap

--- MinWindow.cpp
+++ MinWindow.cpp
@@ -1,40 +1,31 @@
-class Solution {
-public:
-    string minWindow(string s, string t) {
-        if (t.empty()) return "";
 
-        unordered_map<char, int> countT, window;
-        for (char c : t) {
-            countT[c]++;
-        }
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(t) == 0:
+            return "" 
+        res = 0
+        resLen = float("inf")
+        countT = {}
+        for i in range(len(t)):
+            countT[t[i]] = countT.get(t[i], 0) + 1
+        l = 0
+        have = 0
+        need = len(countT)
+        window = {}
+        for r in range(len(s)):
+            c = s[r]
+            window[c] = window.get(c, 0) + 1
+            if c in countT and window[c] == countT[c]:
+                have += 1
+            while have == need:
+                if r - l + 1 < resLen:
+                    resLen = r - l + 1
+                    res = [l, r]
 
-        int have = 0, need = countT.size();
-        pair<int, int> res = {-1, -1};
-        int resLen = INT_MAX;
-        int l = 0;
-
-        for (int r = 0; r < s.length(); r++) {
-            char c = s[r];
-            window[c]++;
-
-            if (countT.count(c) && window[c] == countT[c]) {
-                have++;
-            }
-
-            while (have == need) {
-                if ((r - l + 1) < resLen) {
-                    resLen = r - l + 1;
-                    res = {l, r};
-                }
-
-                window[s[l]]--;
-                if (countT.count(s[l]) && window[s[l]] < countT[s[l]]) {
-                    have--;
-                }
-                l++;
-            }
-        }
-
-        return resLen == INT_MAX ? "" : s.substr(res.first, resLen);
-    }
-};
+                window[s[l]] -= 1
+                if s[l] in countT  and window[s[l]] < countT[s[l]]:
+                    have -= 1
+                l += 1
+        if resLen == float("inf"):
+            return ""
+        return s[res[0]:res[1] + 1]
