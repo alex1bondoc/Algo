26c3cf444e1496ba9bc10a98a64375936c82d142
alex1bondoc <alex1bondoc@gmail.com>
2026-03-25 22:05:49 +0200

    Kth smallest recap

--- KthSmallest.py
+++ KthSmallest.py
@@ -7,19 +7,16 @@
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        stack = []
-        cur = root
-        while stack or cur:
-            while cur:
-                stack.append(cur)
-                cur = cur.left
-            cur = stack.pop()
-            k -= 1
-            if k == 0:
-                return cur.val
-            cur = cur.right
-        return 0
- # Definition for a binary tree node.
+        res = []
+        def dfs(node):
+            if node == None:
+                return
+            dfs(node.left)
+            res.append(node.val)
+            dfs(node.right)
+        dfs(root)
+        return res[k - 1]
+# Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
@@ -28,12 +25,16 @@ class Solution:
 
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        res = []
-        def inorder(node):
-            if not node:
-                return 
-            inorder(node.left)
-            res.append(node.val)
-            inorder(node.right)
-        inorder(root)
-        return res[k - 1]           
+        res = 0
+        stack = []
+        cur = root
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res += 1
+            if res == k:
+                return cur.val
+            cur = cur.right
+        return None
