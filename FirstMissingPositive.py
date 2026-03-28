979ffc1fd1d55569e88b7befd20f08cd8acfe4f8
alex1bondoc <alex1bondoc@gmail.com>
2026-03-28 12:52:41 +0200

    First missing recap

--- FirstMissingPositive.py
+++ FirstMissingPositive.py
@@ -1,17 +1,20 @@
 
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        for i in range(len(nums)):
+        n = len(nums)
+        for i in range(n):
             if nums[i] < 0:
                 nums[i] = 0
-        for i in range(len(nums)):
-            val = abs(nums[i])
-            if 1 <= val <= len(nums):
-                if nums[val - 1] > 0:
-                    nums[val - 1] *= -1
-                elif nums[val - 1] == 0:
-                    nums[val - 1] = -1 * (len(nums) + 1)
-        for i in range(1, len(nums) + 1):
-            if nums[i - 1] >= 0:
-                return i
-        return len(nums) + 1
+        
+        for i in range(n):
+            num = abs(nums[i])
+            if 1 <= num <= n:
+                if nums[num - 1] > 0:
+                    nums[num -1 ] *= -1
+                elif nums[num-1] == 0:
+                    nums[num-1] = -1
+        for i in range(0, n):
+            if nums[i] >= 0:
+                return i + 1
+        return n + 1
+
