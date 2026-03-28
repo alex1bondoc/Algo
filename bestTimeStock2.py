3d9f06485a1b82bf9a479d4e287ba10a94be3010
alex1bondoc <alex1bondoc@gmail.com>
2026-03-28 13:00:29 +0200

    Best time to buy stock

--- bestTimeStock2.py
+++ bestTimeStock2.py
@@ -8,3 +8,15 @@ class Solution:
             dp[i][1] = max(dp[i + 1][1], prices[i] + dp[i + 1][0])
 
         return dp[0][0]
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        res = 0
+        help = float("inf")
+        for num in prices:
+            if num < help:
+                help = num
+            else:
+                res += num - help
+                help = num
+        return res
