f8890ed22fe643bc13c417256dcceb8fe7338dea
alex1bondoc <alex1bondoc@gmail.com>
2026-03-29 18:23:41 +0300

    Best time to buy stock recapt

--- bestTimeStock2.py
+++ bestTimeStock2.py
@@ -19,4 +19,16 @@ class Solution:
             else:
                 res += num - help
                 help = num
-        return res
+ class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        mini = float("inf")
+        res = 0
+        for i in range(len(prices)):
+            price = prices[i]
+            if price < mini:
+                mini = price
+            else:
+                res += price - mini
+                mini = price
+        return res       return res
