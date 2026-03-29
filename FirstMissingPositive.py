e24693afbc83ea5ad1daea8066c4638dd0427532
alex1bondoc <alex1bondoc@gmail.com>
2026-03-29 18:19:21 +0300

    First missing positive recap

--- FirstMissingPositive.py
+++ FirstMissingPositive.py
@@ -1,5 +1,21 @@
 
-class Solution:
+cclass Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        n = len(nums)
+        for i in range(n):
+            nums[i] = max(nums[i], 0)
+        for i in range(n):
+            num = abs(nums[i])
+            if 0 < num <=n:
+                if nums[num - 1] > 0:
+                    nums[num - 1] *= -1
+                elif nums[num - 1] == 0:
+                    nums[num - 1] = -1
+        print(nums)
+        for i in range(n):
+            if nums[i] >= 0:
+                return i + 1
+        return n + 1lass Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         n = len(nums)
         for i in range(n):
